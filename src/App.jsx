05056778c6c54e51adeba3e46eb83e9d691ac3a4
thp728 <thp728@gmail.com>
2023-12-28 12:14:33 +0530

    Completed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import {
   Subscribe,
   SuperQuality,
 } from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <main className="relative">
-      {/* <Nav /> */}
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero />
       </section>
